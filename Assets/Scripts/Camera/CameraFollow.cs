3175ecc01aae59c4a76bf7ac36bdc8235621eb46
opa <skvortsov.ilusha@gmail.com>
2025-09-17 09:32:03 +0200

    Camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float speed = 1.0f;
+
+    void Start()
+    {
+        offset = new Vector3(offset.x, offset.y, transform.position.z);
+    }
+    void LateUpdate()
+    {
+        Vector3 newPosition = Vector3.Lerp(transform.position, target.position + offset, speed * Time.deltaTime);
+        transform.position = newPosition;
+    }
+}
