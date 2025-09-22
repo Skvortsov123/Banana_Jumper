44e78ba2f9af051564e03b557176610ad18e2caf
Blominator <tobias.blomberg@hotmail.com>
2025-09-22 18:48:24 +0200

    Quest 0.1
    Justerade Level Park, började med QuestGiver och QuestChecker

---
+++ Assets/Scripts/Quest/QuestChecker.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestChecker : MonoBehaviour
+{
+    [SerializeField] private GameObject dialougeBox, finishedText, unfinishedText;
+    [SerializeField] private int questGoal = 1;
+    [SerializeField] private int levelToLoad;
+
+    private Animator anim;
+    private bool levelIsLoading = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<SceneChanger>().applesCollected >= questGoal)
+            {
+                dialougeBox.SetActive(true);
+                finishedText.SetActive(true);
+                Invoke("LoadNextLevel", 3.0f);
+                levelIsLoading = true;
+            }
+            else
+            {
+                dialougeBox.SetActive(true);
+                unfinishedText.SetActive(true);
+            }
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !levelIsLoading)
+        {
+            dialougeBox.SetActive(false);
+            finishedText.SetActive(false);
+            unfinishedText.SetActive(false);
+        }
+    }
+}
