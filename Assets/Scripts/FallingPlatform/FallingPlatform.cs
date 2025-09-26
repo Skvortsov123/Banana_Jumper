0c4b417211f4cf14b9abbff9111184d8fad99175
tigergripenberg-debug <tiger.gripenberg@gmail.com>
2025-09-27 01:05:53 +0200

    Level3
    Animation på öppnande plattform. Quest checker, giver och gem

--- Assets/Scripts/FallingPlatform/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform/FallingPlatform.cs
@@ -6,8 +6,8 @@ public class FallingPlatform : MonoBehaviour
 {
     public GameObject Player; // Referens till spelaren
     public string playerTag = "Player"; // Taggen som identifierar spelaren
-    public float disableColliderDelay = 0.3f;  // Tid innan collidern stängs av (när luckan öppnas)
-    public float enableColliderDelay = 1.5f;   // Tid innan collidern aktiveras igen (när luckan stängts)
+    public float disableColliderDelay = 1.5f;  // Tid innan collidern stängs av (när luckan öppnas)
+    public float enableColliderDelay = 1f;   // Tid innan collidern aktiveras igen (när luckan stängts)
 
     private Animator animator;
     private Collider2D platformCollider;
